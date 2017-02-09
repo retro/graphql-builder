e952db43336916ad134323ac17a8b5caef35636a
Mihael Konjevic <konjevic@gmail.com>
2017-02-09 23:51:57 +0100

    Basic project info

--- README.md
+++ README.md
@@ -8,7 +8,6 @@ FIXME
 
 ## License
 
-Copyright © 2017 FIXME
+Copyright Mihael Konjevic (konjevic@gmail.com) © 2017
 
-Distributed under the Eclipse Public License either version 1.0 or (at
-your option) any later version.
+Distributed under the MIT license.
--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject graphql-builder "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject floatingpointio/graphql-builder "0.1.0"
+  :description "GraphQL Query Helper for Clojure(Script)"
+  :url "https://github.com/retro/graphql-builder"
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
                  [graphql-clj "0.1.20"]
                  [funcool/cuerdas "2.0.3"]])
