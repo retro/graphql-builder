6a01861e231abae96ef39d5775dbe47978f9db4d
Mihael Konjevic <konjevic@gmail.com>
2018-06-19 13:04:43 +0200

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject floatingpointio/graphql-builder "0.1.6"
+(defproject floatingpointio/graphql-builder "0.1.7"
   :description "A Clojure(Script) library designed to help with the consuming of GraphQL APIs."
   :url "https://github.com/retro/graphql-builder"
   :license {:name "MIT"
